57ccb0e420ebc67f5f046f77751a30bb48a9db01
Samuel Martinez <semartinez147@gmail.com>
2020-02-24 16:31:14 -0700

    Added sign out activity.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/controller/LoginActivity.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/controller/LoginActivity.java
@@ -1,7 +1,10 @@
 package edu.cnm.deepdive.nasaapod.controller;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import edu.cnm.deepdive.nasaapod.R;
 import edu.cnm.deepdive.nasaapod.service.GoogleSignInRepository;
 
@@ -16,11 +19,29 @@ public class LoginActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     repository = GoogleSignInRepository.getInstance();
     repository.refresh()
-        .addOnSuccessListener((account) -> { /* TODO Switch to main activity */ })
+        .addOnSuccessListener((account) -> switchToMain())
         .addOnFailureListener((ex) -> {
           setContentView(R.layout.activity_login);
           findViewById(R.id.sign_in).setOnClickListener((v) ->
               repository.startSignIn(this, LOGIN_REQUEST_CODE));
         });
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    if (requestCode == LOGIN_REQUEST_CODE) {
+      repository.completeSignIn(data)
+          .addOnSuccessListener((account) -> switchToMain())
+          .addOnFailureListener((ex) ->
+              Toast.makeText(this, R.string.login_failure, Toast.LENGTH_LONG).show());
+    } else {
+      super.onActivityResult(requestCode, resultCode, data);
+    }
+  }
+
+  private void switchToMain() {
+    Intent intent = new Intent(this, MainActivity.class);
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+    startActivity(intent);
   }
 }
